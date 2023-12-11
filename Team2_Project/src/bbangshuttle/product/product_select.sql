f7b938844f6d9ce5242f909dc9190fdf5b789fc9
soohoioa <151589776+soohoioa@users.noreply.github.com>
2023-12-11 21:41:16 +0900

    Delete Team2_Project directory

--- Team2_Project/src/bbangshuttle/product/product_select.sql
+++
@@ -1,4 +0,0 @@
---select 조건
-select * from product where p_no=3;
---select 키워드 검색
-select * from product where p_name like '%라떼%' or p_desc like '%라떼%';
--- Team2_Project/src/bbangshuttle/test/CartDaoTestMain.java
+++
@@ -1,9 +0,0 @@
-package bbangshuttle.test;
-
-public class CartDaoTestMain {
-
-	public static void main(String[] args) throws Exception {
-		
-		
-	}
-}
